78175315833541b4effdf5c1b4add1ab21b8d6e2
kadekriyan <kadekriyan16@gmail.com>
2025-04-21 02:30:00 +0700

    feat: home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/auth/auth_service.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void logout() {
+    final _auth = AuthService();
+    _auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [
+          // logout
+          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
+        ],
+      ),
+    );
+  }
+}
